ede9aa069168fe5ef859341e9f4c43057fcb582e
piyush singh <123636271+itspiyush78@users.noreply.github.com>
2023-07-29 13:06:09 +0530

    conversion operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "piyush" 
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log( valueInNumber);
+
+
+// "33" = > 33
+// "33abcd" = > not a number 
+// true = > 1; false = > 0
+
+let isLoggedIn = "piyush"
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanisLoggedIn);
+
+// 1 = > true; 0 = > false
+// "" = > faalse
+// "piyush" = > true 
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber)
