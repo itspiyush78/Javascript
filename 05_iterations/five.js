4b4346a5a46cf2b1d4adba8ff23aa66d731fb136
piyush singh <123636271+itspiyush78@users.noreply.github.com>
2023-08-30 21:20:28 +0530

    Update five.js

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -5,7 +5,7 @@ const coding = ["js", "ruby", "java", "python", "cpp"]
 // } )
 
 // coding.forEach( (item) => {
-//     console.log(item);
+//     console.log(item)
 // } )
 
 // function printMe(item){
