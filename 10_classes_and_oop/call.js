d1f4dfe787eb4259c8736dfa7790e59c01341709
piyush singh <123636271+itspiyush78@users.noreply.github.com>
2023-09-27 09:22:04 +0530

    Add files via upload

---
+++ 10_classes_and_oop/call.js
@@ -0,0 +1,15 @@
+function SetUsername(username) {
+    // complex DB calls
+    this.username = username
+}
+
+
+function createUser(username, email, password) {
+SetUsername.call(this, username)
+
+this.email = email
+this.password = password
+}
+
+const chai = new createUser("chai", "chai@ig.com", "123")
+console.log(chai);
